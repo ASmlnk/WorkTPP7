8fa18e11a028cbca32d7f72a87018b69b0359c8e
user <sdelanss@gmail.com>
2023-01-22 20:20:04 +0300

    git pull origin master --allow-unrelated-histories

---
+++ app/src/main/java/com/asmlnk/android/asmlnk/worktpp_7/WorkingShift/Compressor.kt
@@ -0,0 +1,12 @@
+package com.asmlnk.android.asmlnk.worktpp_7.WorkingShift
+
+import java.util.*
+
+data class Compressor (val id: UUID = UUID.randomUUID(),
+                      var date: Date = Date(),
+                      var vk1: String = "",
+                      var vk2: String = "",
+                      var vk4: String = "",
+                      var vk5: String = "",
+                      var vk6: String = "") {
+}
