3fff8e5404efa6a25f3fa0b8d28833b078751362
user <sdelanss@gmail.com>
2023-01-25 13:51:36 +0300

    git pull origin master --allow-unrelated-histories

--- app/src/main/java/com/asmlnk/android/asmlnk/worktpp_7/WorkingShift/Compressor.kt
+++ app/src/main/java/com/asmlnk/android/asmlnk/worktpp_7/WorkingShift/Compressor.kt
@@ -1,12 +1,15 @@
 package com.asmlnk.android.asmlnk.worktpp_7.WorkingShift
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.util.*
 
-data class Compressor (val id: UUID = UUID.randomUUID(),
-                      var date: Date = Date(),
-                      var vk1: String = "",
-                      var vk2: String = "",
-                      var vk4: String = "",
-                      var vk5: String = "",
-                      var vk6: String = "") {
+@Entity
+data class Compressor (@PrimaryKey val id: UUID = UUID.randomUUID(),
+                       val date: Date = Date(),
+                       var vk1: String = "",
+                       var vk2: String = "",
+                       var vk4: String = "",
+                       var vk5: String = "",
+                       var vk6: String = "") {
 }
