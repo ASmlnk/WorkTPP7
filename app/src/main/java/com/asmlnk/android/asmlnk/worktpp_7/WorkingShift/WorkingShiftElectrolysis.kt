f34381996ac5f95c709d9d85fe7de442a6cf562d
DelaNx <sdelanss@gmail.com>
2023-02-12 02:58:12 +0300

    git pull origin --allow-unrelated-histories

--- app/src/main/java/com/asmlnk/android/asmlnk/worktpp_7/WorkingShift/WorkingShiftElectrolysis.kt
+++ app/src/main/java/com/asmlnk/android/asmlnk/worktpp_7/WorkingShift/WorkingShiftElectrolysis.kt
@@ -5,19 +5,77 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.asmlnk.android.asmlnk.worktpp_7.R
 import com.asmlnk.android.asmlnk.worktpp_7.databinding.WorkingShiftElectrolysisBinding
 
 class WorkingShiftElectrolysis: Fragment() {
 
     lateinit var binding: WorkingShiftElectrolysisBinding
 
+    private val workingShiftElectrolysisVM: WorkingShiftElectrolysisViewModel by lazy {
+        ViewModelProvider(this) [WorkingShiftElectrolysisViewModel::class.java]
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
         binding = WorkingShiftElectrolysisBinding.inflate(inflater, container, false)
 
+        val listVolumeH = workingShiftElectrolysisVM.listVolumeH
+        val listVolumeN = workingShiftElectrolysisVM.listVolumeN
+
+        binding.spinnerReceiverH1.adapter = arrayAdapter (listVolumeH)
+        binding.spinnerReceiverH2.adapter = arrayAdapter (listVolumeH)
+        binding.spinnerReceiverH3.adapter = arrayAdapter (listVolumeH)
+        binding.spinnerReceiverH4.adapter = arrayAdapter (listVolumeH)
+        binding.spinnerReceiverH5.adapter = arrayAdapter (listVolumeH)
+        binding.spinnerReceiverH6.adapter = arrayAdapter (listVolumeH)
+        binding.spinnerReceiverN1.adapter = arrayAdapter (listVolumeN)
+        binding.spinnerReceiverN2.adapter = arrayAdapter (listVolumeN)
+        binding.spinnerReceiverN3.adapter = arrayAdapter (listVolumeN)
+        binding.spinnerElectrolysis.adapter = ArrayAdapter(
+            requireContext(),
+            R.layout.simple_spinner_dropdown_genrrator_insulation,
+            R.id.text_spinner, workingShiftElectrolysisVM.listElectrolysis)
+
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.spinnerReceiverH1.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+
+            }
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
+        }
+
+        binding.spinnerReceiverH2.onItemSelectedListener = AdapterSpinner()
+    }
+
+    fun arrayAdapter(list: List<Double>): ArrayAdapter<Double> {
+        return ArrayAdapter(
+            requireContext(),
+            R.layout.simple_spinner_dropdown_genrrator_insulation,
+            R.id.text_spinner, list
+        )
+    }
+
+    private inner class AdapterSpinner: AdapterView.OnItemSelectedListener {
+        override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+
+        }
+
+        override fun onNothingSelected(p0: AdapterView<*>?) {
+
+        }
+
+    }
+
     companion object {
         fun newInstance(): WorkingShiftElectrolysis {
             return WorkingShiftElectrolysis()
---
+++ app/src/main/java/com/asmlnk/android/asmlnk/worktpp_7/WorkingShift/WorkingShiftElectrolysisViewModel.kt
@@ -0,0 +1,14 @@
+package com.asmlnk.android.asmlnk.worktpp_7.WorkingShift
+
+import androidx.lifecycle.ViewModel
+
+class WorkingShiftElectrolysisViewModel: ViewModel() {
+
+
+    val listVolumeH = listOf(0.0, 4.0, 4.5, 5.0, 5.5, 6.0, 6.5, 7.0, 7.5, 8.0)
+    val listVolumeN = listOf(0.0, 4.0, 4.5, 5.0, 5.5, 6.0)
+    val listElectrolysis = listOf("", "1", "2")
+
+
+
+}
